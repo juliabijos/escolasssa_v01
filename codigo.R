8fe70574a5b8371afb205f7d61468f58764e6167
juliabijos <juliabijos@outlook.com>
2022-07-18 10:23:21 -0300

    Passo 03 - carregando dplyr

--- codigo.R
+++ codigo.R
@@ -1,3 +1,7 @@
 # Codigo para verificar as maiores e menores escolas
 # Passo 01 - Instalar pacote 
 install.packages("readr")
+
+# Passo 02 
+dados <- read.csv("C:/Users/julia/Documents/R/escolasssa/data/Quant_Geral_Alunos_Escola_Regional_2022.csv", 
+    sep = ";")
