35d35d5552fa24fb783dd0e1989d4cf4d995fab8
juliabijos <juliabijos@outlook.com>
2022-07-18 10:40:11 -0300

    Funcionalidades menores

--- codigo.R
+++ codigo.R
@@ -5,3 +5,9 @@ install.packages("readr")
 # Passo 02 
 dados <- read.csv("C:/Users/julia/Documents/R/escolasssa/data/Quant_Geral_Alunos_Escola_Regional_2022.csv", 
     sep = ";")
+
+# Passo 03 - Carregar dplyr
+library(dplyr)
+
+# Passo 04 - Escolas com menor numero de estudantes
+dados %>% filter(QUANTIDADE < 75)
